c365b1b723423bf886c6bafe5dbc2a7557d91a96
03Res <113329305+03Res@users.noreply.github.com>
2023-11-15 17:02:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/component/resume.jsx
@@ -0,0 +1,47 @@
+const Resume = () =>{
+    return (
+        <div>
+             {/* ======= Resume Section ======= */}
+<section id="resume" className="resume">
+  <div className="container">
+    <div className="section-title">
+      <h2>Resume</h2>
+      <p>Sebagai seorang pengembang UI/UX dan frontend yang berpengalaman, saya memiliki rekam jejak yang telah terbukti dalam memberikan solusi digital yang inovatif. Dengan perhatian yang kuat terhadap desain dan komitmen terhadap pengalaman pengguna yang sempurna, saya ingin sekali menyumbangkan keahlian saya pada proyek-proyek yang dinamis.
+
+      </p>
+    </div>
+    <div className="row">
+      <div className="col-lg-6" data-aos="fade-up">
+        <h3 className="resume-title">Sumary</h3>
+        <div className="resume-item pb-0">
+          <h4>quaresma soruday</h4>
+          <p><em>Desainer UI/UX dan pengembang frontend yang bersemangat dengan mata tajam untuk menciptakan antarmuka yang menarik secara visual dan ramah pengguna. Mampu menerjemahkan ide kompleks menjadi desain yang intuitif dan mengimplementasikannya dengan kode yang bersih dan efisien.</em></p>
+          <ul>
+            <li>airmadidi, sulawesi utara, indonesia</li>
+            <li>0852 9920 5670</li>
+            <li>student@unklab.ac.id</li>
+          </ul>
+        </div>
+        <h3 className="resume-title">Education</h3>
+        <div className="resume-item">
+          <h4>universitas klabat</h4>
+          <h5>2021 - now</h5>
+          <p><em>sulawesi utara, minahasa utara, airmadidi</em></p>
+          <p>Desainer UI/UX dan pengembang frontend yang berbakat dengan dasar kuat yang dibangun melalui tiga setengah tahun studi penuh dedikasi di universitas. Dilengkapi dengan pemahaman menyeluruh tentang prinsip desain dan penguasaan teknologi frontend, saya membawa kombinasi unik antara pengetahuan akademis dan keterampilan praktis untuk menyajikan pengalaman digital yang memikat.</p>
+        </div>
+        <div className="resume-item">
+          <h4>ui/ux &amp; Graphic Design</h4>
+          <h5>2022 - now</h5>
+          <p><em>google devloper student club</em></p>
+          <p>Sebagai mahasiswa di Google Developer Student Club, saya tenggelam dalam pembelajaran desain UI/UX melalui proyek langsung dan kolaborasi. Dengan pengalaman berharga dalam menciptakan antarmuka berorientasi pengguna, serta pengetahuan dalam desain grafis, saya siap untuk masa depan yang dinamis di bidang desain UI/UX.</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  
+</section>{/* End Resume Section */}
+</div>
+    )
+
+}
+export default Resume
